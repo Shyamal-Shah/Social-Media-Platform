160138eb03aa62d37fd0c261424302df84162d42
Shyamal Shah <shahshyamal35@gmail.com>
2024-12-07 12:26:49 +0530

    Added mongodb and redis connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
+const connectMongoDB = require("./config/mongodb");
+const redisClient = require("./config/redis");
 dotenv.config();
-
 
 const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
+connectMongoDB();
+redisClient.connect();
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
